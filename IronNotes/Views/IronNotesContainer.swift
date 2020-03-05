02dadb27dbc88368d2ce50f6dfb240d4ff47b43e
Austin Avery <stewartaavery@gmail.com>
2020-03-04 20:32:18 -0500

    fixing previews

--- IronNotes/Views/IronNotesContainer.swift
+++ IronNotes/Views/IronNotesContainer.swift
@@ -46,7 +46,40 @@ struct IronNotesContainer: View {
 #if DEBUG
 struct IronNotesContainer_Previews: PreviewProvider {
   static var previews: some View {
-    IronNotesContainer()
+    
+    let workout = Workout(context: AppDelegate.viewContext)
+    
+    workout.name = "Test Workout"
+    workout.desc = "Really good workout!"
+    workout.iconName = "barbell"
+    workout.lastWorkoutDate = Date()
+    
+    let exerciseDetail = ExerciseDetail(context: AppDelegate.viewContext)
+    exerciseDetail.name = "Test"
+    exerciseDetail.exerciseDetailIndex = 0
+    
+    let exerciseSet = ExerciseSet(context: AppDelegate.viewContext)
+    exerciseSet.exerciseSetIndex = 0
+    exerciseSet.reps = 3
+    exerciseSet.weight = 2
+    exerciseDetail.addToSets(exerciseSet)
+    
+    workout.addToRoutines(exerciseDetail)
+    
+    let exerciseDetail2 = ExerciseDetail(context: AppDelegate.viewContext)
+    exerciseDetail2.name = "Test 2"
+    exerciseDetail2.exerciseDetailIndex = 1
+    
+    let exerciseSet2 = ExerciseSet(context: AppDelegate.viewContext)
+    exerciseSet2.exerciseSetIndex = 1
+    exerciseSet2.reps = 5
+    exerciseSet2.weight = 39
+    exerciseDetail2.addToSets(exerciseSet2)
+    
+    workout.addToRoutines(exerciseDetail2)
+    
+    return IronNotesContainer().environment(\.managedObjectContext, AppDelegate.viewContext)
+    
   }
 }
 #endif
