51e3713cf41291b7209550b864f8b19647ab9c16
Austin Avery <stewartaavery@gmail.com>
2020-03-15 23:12:23 -0400

    start creating new workout modal

--- IronNotes/Views/ConfigureWorkout/NewWorkout.swift
+++ IronNotes/Views/ConfigureWorkout/NewWorkout.swift
@@ -9,13 +9,49 @@
 import SwiftUI
 
 struct NewWorkout: View {
+  @Binding var isPresented: Bool
+  
+  @State var name: String = ""
+  @State var description: String = ""
   var body: some View {
-    Text("New workout!")
+    
+    VStack {
+      VStack {
+        HStack {
+          Button(action: {self.isPresented.toggle()})  {
+            Text("Cancel")
+          }
+          Spacer()
+          Text("New Workout")
+            .fontWeight(.bold)
+          Spacer()
+          Button(action: {self.isPresented.toggle()})  {
+            Text("Done")
+              .foregroundColor(Color.blue)
+              .fontWeight(.bold)
+          }
+        }.padding()
+          .padding(.bottom, 0)
+        Spacer()
+        Divider()
+      }.frame(height: 45)
+      Form {
+        Section {
+          TextField("Workout name", text: $name)
+          TextField("Description", text: $description)
+        }
+        Section {
+          TextField("Other", text: $name)
+        }
+      }
+    }
   }
 }
 
 struct NewWorkout_Previews: PreviewProvider {
+  @State static var isModalPresented = true
   static var previews: some View {
-    NewWorkout()
+    NewWorkout(isPresented: $isModalPresented)
   }
+  
 }
